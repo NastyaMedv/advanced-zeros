152ebc3d7b282194c5845b9877b8c8cd6a8b1fae
NastyaMedv <NastyaMedv@gmail.com>
2018-03-02 00:57:20 +0300

    «solution»

--- src/index.js
+++ src/index.js
@@ -1,27 +1,62 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+  var zero = 0, N = base, mn = [], k = 2, j = 0;
 
-  var zero = 0;
-  var b = 5;
-  if (base % 2 == 0) { b = base / 2; }
-    else { b = (base-1) / 2;}
-
-  if (base == 2) { return 0;}
-  if (base == 3) { return 0;}
-
-  var i = b, j = b;
-  while (i <= number) {
-    j = i;
-    while ((j % b) == 0) {
-      j = j / b;
-      zero++;
+  while ((N > 1) && (k <= N)) {
+    if (N % k == 0) {
+      mn[j] = k;
+      j++;
+      N = N / k;
+    } else {
+      if (k == 2) {
+        k++;
+      } else {
+        k += 2;
+      }
     }
-    i = i + b;
   }
 
-  for (var z=0; z<zero; z++) {
-
+  var mnoj = [[],[],[],[]];
+  mnoj[0][0] = mn[0];
+  mnoj[1][0] = 0;
+  mnoj[2][0] = 0;
+  j=0;
+  for (var i = 0; i < mn.length; i++) {
+    if (mn[i] == mnoj[0][j]) {
+      mnoj[1][j]++;
+    } else {
+      j++;
+      mnoj[0][j] = mn[i];
+      mnoj[1][j] = 1;
+      mnoj[2][j] = 0;
+    }
   }
 
-  return zero;
+  for (var i = 2; i <= number; i++) {
+    for (var x = 0; x < mnoj[0].length; x++) {
+      if ((i % mnoj[0][x]) == 0) {
+        j = i;
+        while ((j % mnoj[0][x]) == 0) {
+          j = j / mnoj[0][x];
+          mnoj[2][x]++;
+        }
+      }
+    }
+  }
+
+  for (var i = 0; i < mnoj[0].length; i++) {
+    mnoj[3][i] = mnoj[2][i] / mnoj[1][i];
+  }
+
+  zero = mnoj[3][0];
+  for (var i = 1; i < mnoj[0].length; i++) {
+    if (mnoj[3][i] < zero) {
+      zero = mnoj[3][i];
+    }
+  }
+
+
+
+
+  return Math.floor(zero);
 }
