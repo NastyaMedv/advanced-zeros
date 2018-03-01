564d1bb50e1a4f3a5b40ada18ddce71cede84fca
NastyaMedv <NastyaMedv@gmail.com>
2018-03-01 19:01:25 +0300

    «solution»

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+
+  var zero = 0;
+  var b = 5;
+  if (base % 2 == 0) { b = base / 2; }
+    else { b = (base-1) / 2;}
+
+  var i = b, j = b;
+  while (i <= number) {
+    j = i;
+    while ((j % b) == 0) {
+      j = j / b;
+      zero++;
+    }
+    i = i + b;
+  }
+
+  for (var z=0; z<zero; z++) {
+
+  }
+
+  return zero;
 }
